1a75e8916948ae5fe20cd45e0c9bb675d89d08c8
Inverter Team <154754910+DanilVusenko86@users.noreply.github.com>
2024-03-25 15:36:59 +0200

    Add files via upload

---
+++ system/theme/Progressbar/progrrsss.bat
@@ -0,0 +1,154 @@
+echo off
+
+cls 
+
+type load.txt
+
+
+timeout  /t 1
+cls 
+
+type load1.txt
+
+
+timeout /t 1
+
+cls 
+
+type load3.txt
+
+
+timeout /t 1
+
+cls 
+
+type load4.txt
+
+
+timeout /t 1
+
+cls 
+
+type load5.txt
+
+
+timeout /t 1
+
+cls 
+
+type load6.txt
+
+
+timeout /t 1
+
+cls 
+
+type load7.txt
+
+
+timeout /t 1
+
+cls 
+
+type load8.txt
+
+
+timeout /t 1
+
+cls 
+
+type load9.txt
+
+
+timeout /t 1
+
+cls 
+
+type load10.txt
+
+
+timeout /t 1
+
+cls 
+
+type load11.txt
+
+
+timeout /t 1
+
+cls 
+
+type load12.txt
+
+
+timeout /t 1
+
+cls 
+
+type load13.txt
+
+
+timeout /t 1
+
+cls 
+
+type load14.txt
+
+
+timeout /t 1
+
+cls 
+
+type load15.txt
+
+
+timeout /t 1
+
+cls 
+
+type load16.txt
+
+
+timeout /t 1
+
+cls 
+
+type load17.txt
+
+
+timeout /t 1
+
+cls 
+
+type load18.txt
+
+
+timeout /t 1
+
+cls 
+
+type load19.txt
+
+
+timeout /t 1
+
+cls 
+
+type load20.txt
+
+
+timeout /t 1
+
+cls 
+
+type load21.txt
+
+
+timeout /t 1
+
+cls 
+
+type load22.txt
+
+
+timeout /t 1
